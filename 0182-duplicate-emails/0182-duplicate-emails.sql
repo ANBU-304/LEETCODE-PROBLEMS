eb36dab91e48e8e0c9ab2eb76a82853a43427e51
Anbumani P <anbuarul401@gmail.com>
2026-02-04 12:20:05 +0530

    Time: 339 ms (82.53%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 #select p.email as Email from Person p join Person e on p.id=e.id where p.email extent  e.email; 
 
-select email as Email from Person group by email having(count(*)>1) ;
+select email as Email from Person group by email having(count(email)>1) ;
