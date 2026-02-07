c56991578e18c8d71065f4bb0cd284a21d02169c
Anbumani P <anbuarul401@gmail.com>
2026-02-07 10:12:08 +0530

    Time: 6 ms (40.06%), Space: 46.8 MB (40.29%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -3,11 +3,10 @@ class Solution {
     Arrays.sort(seats);
     Arrays.sort(students);
     int sum=0;
-    int k=0;
     for(int i=0;i<seats.length;i++)
     {
-        sum+=Math.abs(seats[i]-students[k]);
-        k++;
+        sum+=Math.abs(seats[i]-students[i]);
+     
     }
     return sum;
     }
