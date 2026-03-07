9ac993aab12fcdaa3d67458ad3c3e31ffa9a717d
Anbumani P <anbuarul401@gmail.com>
2026-03-07 22:36:43 +0530

    Time: 3 ms (5.86%), Space: 43.7 MB (5.09%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String toLowerCase(String s) {
+    String str="";
+    for(char c:s.toCharArray()){
+        if(c>='A' && c<='Z'){
+            str+=(char)(c+32);
+        }
+        else{
+            str+=c;
+        }
+    }
+    return str; 
+    }
+}
