edc7276a8254d96e2bedbc04a593c15ed320c118
Anbumani P <anbuarul401@gmail.com>
2026-03-18 18:21:47 +0530

    Time: 3 ms (44.62%), Space: 44 MB (44.25%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -10,7 +10,7 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) != goal.charAt(i)) {
 
-                if (k == 2) // ✅ FIX HERE
+                if (k == 2) 
                     return false;
 
                 a[k] = i;
