6e9fbf2f55dd21756099b20a250f4c7612485a4a
Anbumani P <anbuarul401@gmail.com>
2026-03-23 14:02:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -3,12 +3,12 @@ class Solution {
         
        for(int i=0;i<nums.length;i++)
        {
-       
+        int a=nums[i];
         int sum=0;
-        while(nums[i]>0)
+        while(a>0)
         {
-            sum+=nums[i]%10;
-            nums[i]/=10;
+            sum+=a%10;
+            a/=10;
             
         }
         if(sum==i)
