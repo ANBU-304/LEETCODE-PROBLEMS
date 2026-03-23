c4685505634399e558567660d557192a326100c6
Anbumani P <anbuarul401@gmail.com>
2026-03-23 11:04:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        
+        int low = 0;
+        int sum = 0;
+
+        for (int w : weights) {
+            low = Math.max(low, w); // ✅ max element
+            sum += w;
+        }
+
+        int high = sum;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+
+            if (helper(weights, mid) <= days) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public int helper(int[] weights, int mid) {
+        int currSum = 0;
+        int d = 1;
+
+        for (int num : weights) {
+            if (currSum + num > mid) { // ✅ fixed
+                d++;
+                currSum = num;
+            } else {
+                currSum += num;
+            }
+        }
+        return d;
+    }
+}
