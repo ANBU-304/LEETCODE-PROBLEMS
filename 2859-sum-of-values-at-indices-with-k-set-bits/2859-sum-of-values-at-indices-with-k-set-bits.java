7cfa6cbaca472b5c8a588409168388d22c53220d
Anbumani P <anbuarul401@gmail.com>
2026-03-25 10:39:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.java
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.java
@@ -1,21 +1,13 @@
 class Solution {
     public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
-        int sum=0;
-        
+        int sum = 0;
+
         for (int i = 0; i < nums.size(); i++) {
-            int count=0;
-            for (int j = 0; j<32; j++) {
-                if ((i & (1<<j)) != 0) {
-                    count++;
-                }
-                
+            if (Integer.bitCount(i) == k) {
+                sum += nums.get(i);
             }
-            if (count == k) {
-                    sum += nums.get(i);
-                   
-                }
-                
         }
+
         return sum;
     }
 }
