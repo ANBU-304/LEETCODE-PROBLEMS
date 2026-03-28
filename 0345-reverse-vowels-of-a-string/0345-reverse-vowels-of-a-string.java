7b13bbe3503f4621f973aef831cc79e9dd829b3f
Anbumani P <anbuarul401@gmail.com>
2026-03-28 23:09:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,44 +1,35 @@
 class Solution {
+    
+    public boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    
     public String reverseVowels(String s) {
-        int c=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if((s.charAt(i)== 'A' )||(s.charAt(i)== 'a') ||(s.charAt(i)== 'E' )||( s.charAt(i)== 'e') ||( s.charAt(i)== 'I') || (s.charAt(i)== 'i') || (s.charAt(i)== 'O')|| s.charAt(i)== 'U' || s.charAt(i)== 'u' || (s.charAt(i)== 'o') )
-            {
-                c++;
+        char[] arr = s.toCharArray();
+        
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left < right) {
+            
+            while (left < right && !isVowel(arr[left])) {
+                left++;
             }
-        }
-        char [] v = new char [c];
-        int k=0;
-        for(int i=0;i<s.length();i++)
-        {
-           if((s.charAt(i)== 'A' )||(s.charAt(i)== 'a') ||(s.charAt(i)== 'E' )||( s.charAt(i)== 'e') ||( s.charAt(i)== 'I') || (s.charAt(i)== 'i') || (s.charAt(i)== 'O')|| s.charAt(i)== 'U' || s.charAt(i)== 'u'|| (s.charAt(i)== 'o') )
-            {
-                v[k]=s.charAt(i);
-                k++;
+            
+            while (left < right && !isVowel(arr[right])) {
+                right--;
             }
+            
+            // swap vowels
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            
+            left++;
+            right--;
         }
-
-        char []ans = s.toCharArray();
-
-        k=k-1;
-
-
-        for(int i=0;i<s.length();i++)
-        {
-           if((s.charAt(i)== 'A' )||(s.charAt(i)== 'a') ||(s.charAt(i)== 'E' )||( s.charAt(i)== 'e') ||( s.charAt(i)== 'I') || (s.charAt(i)== 'i') ||(s.charAt(i)== 'o') || (s.charAt(i)== 'O')|| s.charAt(i)== 'U' || s.charAt(i)== 'u' )
-            {
-                ans[i]=v[k];
-                k--;
-            }
-        }
-        String res=new String();
-
-        for(int i=0;i<s.length();i++)
-        {
-            res+=ans[i];
-        }
-
-        return res;
+        
+        return new String(arr);
     }
 }
