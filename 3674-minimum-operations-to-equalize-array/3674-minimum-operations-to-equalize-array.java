fcefddaf3b74a5ddf6e07733995f867ee0a96d6a
Anbumani P <anbuarul401@gmail.com>
2026-06-03 10:37:22 +0530

    Time: 1 ms (68.28%), Space: 45.3 MB (30.22%) - LeetHub

---
+++ 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count=0;
+
+        for(int n : nums)
+        {
+            if(n==nums[0])
+            {
+                count++;
+            }
+            
+        }
+
+        return count<nums.length?1:0;
+    }
+}
