21b6ec1c65b8f76fd6b98cf1606e2c43639ff66e
Anbumani P <anbuarul401@gmail.com>
2026-06-05 10:01:16 +0530

    Time: 15 ms (63.04%), Space: 51.3 MB (85.46%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,17 +2,8 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         int k=0;
         int count=0;
-     if(s.length == 0)return 0;
-        Thread t1 = new Thread(() -> {Arrays.sort(g);});
-        Thread t2 = new Thread(() -> {Arrays.sort(s);});
-        t1.start();
-        t2.start();
-        try {
-        t1.join();
-        t2.join();
-        }
-        catch(InterruptedException e) {
-        }
+        Arrays.sort(g);
+        Arrays.sort(s);
         int i=0;
         while(i<g.length && k<s.length)
         {
