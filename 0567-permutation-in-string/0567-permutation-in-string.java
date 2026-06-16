bf95bbdc9a8353e8ef72db29beb3993625423061
Anbumani P <anbuarul401@gmail.com>
2026-06-16 09:31:36 +0530

    Time: 793 ms (5.02%), Space: 47.2 MB (5.99%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,26 +1,22 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         char[] a = s1.toCharArray();
         Arrays.sort(a);
-        for(int i=0;i<= s2.length() - s1.length();i++)
-        {
-            StringBuffer ans = new StringBuffer();
-            for(int j=i;j<i+s1.length();j++)
-            {
-                ans.append(s2.charAt(j));
-            }
-             char[] b = ans.toString().toCharArray();
+        String target = new String(a);
+
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            String sub = s2.substring(i, i + s1.length());
+
+            char[] b = sub.toCharArray();
             Arrays.sort(b);
 
-            if(Arrays.equals(a, b)) {
+            if (target.equals(new String(b))) {
                 return true;
             }
-
         }
+
         return false;
-
-        
-
-        
     }
 }
