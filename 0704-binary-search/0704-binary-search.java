eba42d1ddbae6b18e3697247e3030cb7ae3ff48c
Anbumani P <anbuarul401@gmail.com>
2026-06-17 10:04:00 +0530

    Time: 0 ms (100%), Space: 48.6 MB (13.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,26 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
     
-        int left = 0, right = nums.length - 1;
-        
-        while (left <= right) {
-        
-            int mid = left + (right - left) / 2;
-            
-      
-            if (nums[mid] == target) {
-                return mid;
-            } 
-      
-            else if (nums[mid] < target) {
-                left = mid + 1;   
-            } 
-         
-            else {
-                right = mid - 1;
-            }
-        }
+        int ans =Arrays.binarySearch(nums, target);
 
+        if(ans<0)
         return -1;
+
+        return ans;
     }
 }
