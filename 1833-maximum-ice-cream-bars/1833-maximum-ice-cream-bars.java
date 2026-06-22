15361d59e55772d771de495a28b7dbac9feeaa47
Anbumani P <anbuarul401@gmail.com>
2026-06-22 09:20:56 +0530

    Time: 38 ms (24.02%), Space: 84.1 MB (28.26%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int i=0;i<costs.length;i++)
+        {
+            
+            if(coins>0 && costs[i]<=coins)
+            {
+                count++;
+               coins = coins- costs[i];
+            }else
+            {
+                break;
+            }
+        }
+        return count;
+    }
+}
