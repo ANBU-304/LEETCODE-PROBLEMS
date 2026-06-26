4e35556f07ce736648cb8ea8eae14d4eb74789c5
Anbumani P <anbuarul401@gmail.com>
2026-06-26 19:22:02 +0530

    Time: 1 ms (99.82%), Space: 48.2 MB (95.75%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,7 +1,16 @@
 class Solution {
-    public int[] countBits(int n) {
-         int[] f = new int[n + 1];
-        for (int i=1; i<=n; i++) f[i] = f[i >> 1] + (i & 1);
-    return f;
+    static{
+        for(int i=0; i<300;i++)
+            countBits(i);
+    }
+    static public int[] countBits(int n) {
+
+        int[] ans = new int[n+1];
+
+        for(int i = 1 ; i <= n ; i++){
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+
+        return ans;
     }
 }
