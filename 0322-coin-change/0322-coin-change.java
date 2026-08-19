b644fda88db10d68a0311e950b9ad1ff2b0e9051
Anbumani P <anbuarul401@gmail.com>
2026-08-19 09:42:02 +0530

    Time: 14 ms (91.14%), Space: 46.1 MB (93.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,25 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
+        int[] dp = new int[amount + 1];
 
-        // Base Case: 0 coins needed for amount 0
-        for (int i = 0; i <= n; i++) dp[i][0] = 0;
-        // Base Case: Infinity coins for amount > 0 with 0 coin types
-        for (int j = 1; j <= amount; j++) dp[0][j] = Integer.MAX_VALUE - 1;
+        // Fill with a value larger than any possible answer
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = amount + 1;
+        }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (coins[i - 1] <= j) {
-                    // Min of (Include current coin + 1, Exclude current coin)
-                    dp[i][j] = Math.min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        
-        int result = dp[n][amount];
-        return result >= Integer.MAX_VALUE - 1 ? -1 : result;
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
